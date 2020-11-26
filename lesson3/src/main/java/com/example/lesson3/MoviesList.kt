9fa78cf3201d88eb0473554fd68668556cad1890
paxett <y.bogdanov@gmail.com>
2020-11-26 23:07:35 +0300

    Navigation between MoviesList and MovieDetails fragments added. Navigation component not used yet.

--- lesson3/src/main/java/com/example/lesson3/MoviesList.kt
+++ lesson3/src/main/java/com/example/lesson3/MoviesList.kt
@@ -4,25 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MoviesList.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MoviesList : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
+        view.findViewById<ImageView>(R.id.bg).apply {
+            setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.parent_fragment, MovieDetails())
+                    .commit()
+            }
+        }
+        return view
     }
 }
