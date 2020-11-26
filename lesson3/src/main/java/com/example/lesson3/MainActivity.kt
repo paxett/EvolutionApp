cae479de172b7f8fd5e7c5684b930ed5d877007b
paxett <y.bogdanov@gmail.com>
2020-11-26 17:44:30 +0300

    Added MoviesList fragment. MovieDetails fragment put into parent MoviesList fragment. MovieDetails fragment layout removed from main activity, now it's inflated by fun onCreate.

--- lesson3/src/main/java/com/example/lesson3/MainActivity.kt
+++ lesson3/src/main/java/com/example/lesson3/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.lesson3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.parent_fragment, MoviesList())
+                .commit()
+        }
     }
 }
