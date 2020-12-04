5d09ece2eea4f404416ef49a94a2d071f3bd2ec2
paxett <y.bogdanov@gmail.com>
2020-12-04 14:05:32 +0300

    Lesson 4 Initial commit

---
+++ lesson4/src/main/java/com/paxet/evoapp/lesson4/MoviesList.kt
@@ -0,0 +1,23 @@
+package com.paxet.evoapp.lesson4
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+
+class MoviesList : Fragment(R.layout.fragment_movies_list) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<ImageView>(R.id.bg).apply {
+            setOnClickListener {
+                findNavController().navigate(R.id.action_moviesList_to_movieDetails)
+//      Use NavController instead of Fragment Manager
+//                parentFragmentManager.beginTransaction()
+//                    .addToBackStack(null)
+//                    .replace(R.id.parent_fragment, MovieDetails())
+//                    .commit()
+            }
+        }
+    }
+}
