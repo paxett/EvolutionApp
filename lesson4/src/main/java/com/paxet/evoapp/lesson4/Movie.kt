c0245bea35ba6ea093a1de0ae5e0e3585fd4f849
paxett <y.bogdanov@gmail.com>
2020-12-05 14:56:30 +0300

    Classes Movie and Actor added. Resources updated.

---
+++ lesson4/src/main/java/com/paxet/evoapp/lesson4/Movie.kt
@@ -0,0 +1,12 @@
+package com.paxet.evoapp.lesson4
+
+data class Movie(
+    val name : String,
+    val poster : String,
+    val genres: String,
+    val reviews_counter : String,
+    val rating : Byte,
+    val duration: String,
+    val age : String,
+    val like : Boolean
+)
