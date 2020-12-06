e596fd68f03d8833d64420a4e19b9125b688aae7
paxett <y.bogdanov@gmail.com>
2020-12-06 11:31:05 +0300

    NavController moved to MoviesAdapter ctor params

--- lesson4/src/main/java/com/paxet/evoapp/lesson4/MoviesAdapter.kt
+++ lesson4/src/main/java/com/paxet/evoapp/lesson4/MoviesAdapter.kt
@@ -4,15 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 
 class MoviesAdapter(
     context : Context,
     var movies : List<Movie>,
+    val navController : NavController
 ) : RecyclerView.Adapter<MovieViewHolder>() {
     private val inflater: LayoutInflater = LayoutInflater.from(context)
-    private  var navController: NavController? = null
 
     override fun getItemCount(): Int = movies.size
     fun getItem(position: Int): Movie = movies.get(position)
@@ -25,7 +24,6 @@ class MoviesAdapter(
         holder.bind(movies.get(position))
         if(position == 0) {
             holder.itemView.setOnClickListener {
-                navController = Navigation.findNavController(holder.itemView)
                 navController!!.navigate(R.id.action_moviesList_to_movieDetails)
             }
         }
