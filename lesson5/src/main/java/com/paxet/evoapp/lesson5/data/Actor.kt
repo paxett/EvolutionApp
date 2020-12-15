f083b9071d54fcda03cf4cd0c2c21f4c9545b01d
paxett <y.bogdanov@gmail.com>
2020-12-15 13:10:10 +0300

    Data classes (Actor,Genre,Movie,JsonLoader) and assets added. Serialization and Coroutines dependenciees added to build.gradle

---
+++ lesson5/src/main/java/com/paxet/evoapp/lesson5/data/Actor.kt
@@ -0,0 +1,7 @@
+package com.paxet.evoapp.lesson5.data
+
+data class Actor(
+    val id: Int,
+    val name: String,
+    val picture: String
+)
---
+++ lesson5/src/main/java/com/paxet/evoapp/lesson5/data/Genre.kt
@@ -0,0 +1,3 @@
+package com.paxet.evoapp.lesson5.data
+
+data class Genre(val id: Int, val name: String)
