a18ff913b7b10b3f9e380228835f2427f069e238
paxett <y.bogdanov@gmail.com>
2021-01-08 17:16:37 +0300

    ViewHolders updated with TMDB API fields.

--- lesson7/src/main/java/com/paxet/evoapp/lesson7/ui/fragments/actors/ActorViewHolder.kt
+++ lesson7/src/main/java/com/paxet/evoapp/lesson7/ui/fragments/actors/ActorViewHolder.kt
@@ -1,4 +1,4 @@
-package com.paxet.evoapp.lesson7.ui.fragments.moviedetails
+package com.paxet.evoapp.lesson7.ui.fragments.actors
 
 import android.view.View
 import android.widget.ImageView
@@ -6,15 +6,18 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.paxet.evoapp.lesson7.R
-import com.paxet.evoapp.lesson7.data.Actor
+import com.paxet.evoapp.lesson7.data.network.NetworkModule
+import com.paxet.evoapp.lesson7.data.tmdbapi.CastItem
 
 class ActorViewHolder(view : View) : RecyclerView.ViewHolder(view) {
     private val avatar : ImageView = itemView.findViewById(R.id.avatar)
     private val actor_name : TextView = itemView.findViewById(R.id.actor_name)
     private val view = view
 
-    fun bind(actor : Actor) {
-        Glide.with(view).load(actor.picture).into(avatar)
-        actor_name.text = actor.name
+    fun bind(actor : CastItem?) {
+        Glide.with(view)
+            .load("${NetworkModule.baseImageUrl}/w92/${actor?.profilePath}").into(avatar)
+
+        actor_name.text = actor?.name ?: ""
     }
 }
--- lesson7/src/main/java/com/paxet/evoapp/lesson7/ui/fragments/movieslist/MovieViewHolder.kt
+++ lesson7/src/main/java/com/paxet/evoapp/lesson7/ui/fragments/movieslist/MovieViewHolder.kt
@@ -6,10 +6,14 @@ import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.paxet.evoapp.lesson7.R
-import com.paxet.evoapp.lesson7.data.Movie
+import com.paxet.evoapp.lesson7.data.network.NetworkModule
+import com.paxet.evoapp.lesson7.data.tmdbapi.MovieItemAPI
 
-class MovieViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+class MovieViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val poster : ImageView = itemView.findViewById(R.id.color_poster)
     private val like_enabled : ImageView = itemView.findViewById(R.id.like_enabled)
     private val like_disabled : ImageView = itemView.findViewById(R.id.like_disabled)
@@ -21,13 +25,21 @@ class MovieViewHolder(view : View) : RecyclerView.ViewHolder(view) {
     private val ratingBar : RatingBar = itemView.findViewById(R.id.ratingBar)
     private val view = view
 
-    fun bind(movie : Movie) {
-        Glide.with(view).load(movie.poster).into(poster)
+    fun bind(movie: MovieItemAPI) {
+        Glide.with(view)
+            .load("${NetworkModule.baseImageUrl}/w154/${movie.posterPath}")
+            .apply(RequestOptions.bitmapTransform(RoundedCorners(150)))
+            .into(poster)
+
         title.text = movie.title
-        genres.text = movie.genres.joinToString { it.name }
-        reviews_counter.text = movie.numberOfRatings.toString()
-        age.text = movie.minimumAge.toString() + "+"
-        ratingBar.rating = movie.ratings
-        duration.text = movie.runtime.toString()
+        //TODO change this
+        genres.text = movie.genreIds?.joinToString { it.toString() } ?: ""
+        reviews_counter.text = movie.voteCount.toString()
+        //TODO do not see age restrictions in TMDB API
+        age.text = if(movie.adult == true) "18+" else "0+"
+        //TODO check if works correct, calibrate to 5 stars indicator
+        ratingBar.rating = movie.voteAverage?.toFloat() ?: 0f
+        //TODO do not see movie duration in TMDB API
+        duration.text = ""
     }
 }
