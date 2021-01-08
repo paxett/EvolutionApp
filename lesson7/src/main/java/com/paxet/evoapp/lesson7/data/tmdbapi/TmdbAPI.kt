86923aa251a857053cf4b037d7c5ef6bff8d8bc8
paxett <y.bogdanov@gmail.com>
2021-01-08 17:11:57 +0300

    Added TMDB API data classes. Removed old data classes.

---
+++ lesson7/src/main/java/com/paxet/evoapp/lesson7/data/tmdbapi/TmdbAPI.kt
@@ -0,0 +1,31 @@
+package com.paxet.evoapp.lesson7.data.tmdbapi
+
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface TmdbAPI {
+
+    @GET("configuration")
+    suspend fun getAPIConfiguration(
+        @Query("api_key") apiKey : String
+    ) : ConfigurationAPI
+
+    @GET("movie/now_playing")
+    suspend fun getNowPlaying(
+        @Query("api_key") apiKey : String
+    ) : MoviesListAPI
+
+    @GET("movie/{movie_id}")
+    suspend fun getMovieDetails(
+        @Path("movie_id")  movieId : String,
+        @Query("api_key") apiKey : String
+    ) : MovieDetailsAPI
+
+    @GET("movie/{movie_id}/credits")
+    suspend fun getMovieCredits(
+        @Path("movie_id")  movieId : String,
+        @Query("api_key") apiKey : String
+    ) : MovieCreditsAPI
+}
+
---
+++ lesson7/src/main/java/com/paxet/evoapp/lesson7/ui/fragments/BaseVM.kt
@@ -0,0 +1,26 @@
+package com.paxet.evoapp.lesson7.ui.fragments
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.paxet.evoapp.lesson7.data.network.NetworkModule
+import com.paxet.evoapp.lesson7.data.network.NetworkModule.tmdbAPI
+import kotlinx.coroutines.*
+
+open class BaseVM : ViewModel() {
+    val coroutineScope = CoroutineScope(Job() + Dispatchers.IO)
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        Log.e(BaseVM.TAG, "Coroutine exception, scope active:${coroutineScope.isActive}", throwable)
+    }
+
+    fun initConfiguration() {
+        coroutineScope.launch(exceptionHandler) {
+            NetworkModule.baseImageUrl = tmdbAPI.getAPIConfiguration(apiKey).images?.secureBaseUrl ?: ""
+        }
+    }
+
+
+    companion object {
+        private val TAG = BaseVM::class.java.simpleName
+        val apiKey = "c9e69769a0b528e00cf6da3c3199eb0e"
+    }
+}
