970aec396b28218e5e7041b3e396e47a89e9915c
paxett <y.bogdanov@gmail.com>
2021-01-10 13:44:32 +0300

    Back button onClickListener added to MovieDetailsFragment.

--- lesson7/src/main/java/com/paxet/evoapp/lesson7/ui/fragments/moviedetails/MovieDetailsFragment.kt
+++ lesson7/src/main/java/com/paxet/evoapp/lesson7/ui/fragments/moviedetails/MovieDetailsFragment.kt
@@ -10,6 +10,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -29,6 +30,13 @@ class MovieDetailsFragment : Fragment(R.layout.fragment_movie_details) {
             layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.HORIZONTAL, false)
         }
         viewModel.initMovie(arguments)
+
+        view.findViewById<TextView>(R.id.back_txt).setOnClickListener {
+            view.findNavController().navigate(R.id.action_movieDetails_to_moviesList)
+        }
+        view.findViewById<ImageView>(R.id.back_img).setOnClickListener {
+            view.findNavController().navigate(R.id.action_movieDetails_to_moviesList)
+        }
     }
 
     private fun initObservers(view: View) {
@@ -41,9 +49,9 @@ class MovieDetailsFragment : Fragment(R.layout.fragment_movie_details) {
         val storyLine: TextView = view.findViewById(R.id.body)
 
         viewModel.movieLD.observe(viewLifecycleOwner, Observer { movie ->
-            movie?.first.run {
+            movie.first.run {
                 Glide.with(view)
-                        .load("${NetworkModule.baseImageUrl}/w342/${this?.backdropPath}")
+                        .load("${NetworkModule.baseImageUrl}/w342/${backdropPath}")
                         .into(bw_poster)
 
                 val colorMatrix = ColorMatrix()
@@ -51,16 +59,16 @@ class MovieDetailsFragment : Fragment(R.layout.fragment_movie_details) {
                 val filter = ColorMatrixColorFilter(colorMatrix)
                 bw_poster.setColorFilter(filter)
 
-                age.text = if (this?.adult == true) "18+" else "0+"
-                name.text = this?.title ?: ""
-                genresTV.text = this?.genres?.joinToString { it?.name.toString() } ?: ""
-                rating.rating = this?.voteAverage?.toFloat() ?: 0f
-                reviewCounter.text = this?.voteCount.toString()
-                storyLine.text = this?.overview ?: ""
+                age.text = if (adult == true) "18+" else "0+"
+                name.text = title ?: ""
+                genresTV.text = genres?.joinToString { it?.name.toString() } ?: ""
+                rating.rating = voteAverage?.toFloat() ?: 0f
+                reviewCounter.text = voteCount.toString()
+                storyLine.text = overview ?: ""
             }
 
-            movie?.second.run {
-                actorsAdapter.actors = this?.cast ?: listOf()
+            movie.second.run {
+                actorsAdapter.actors = cast ?: listOf()
             }
         })
     }
