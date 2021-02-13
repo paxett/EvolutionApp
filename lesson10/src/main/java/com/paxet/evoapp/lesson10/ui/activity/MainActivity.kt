4d9cd8edf63a1f039548f597638cca7747dfbdc8
paxett <y.bogdanov@gmail.com>
2021-02-13 21:56:43 +0300

    Implicit deep link to movie details fragment added

--- lesson10/src/main/java/com/paxet/evoapp/lesson10/ui/activity/MainActivity.kt
+++ lesson10/src/main/java/com/paxet/evoapp/lesson10/ui/activity/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.paxet.evoapp.lesson10.ui.activity
 
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.paxet.evoapp.lesson10.R
 
 class MainActivity : AppCompatActivity() {
@@ -15,5 +17,19 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        when (intent.action) {
+            // Invoked when a dynamic shortcut is clicked.
+            Intent.ACTION_VIEW -> {
+                val id = intent.data?.lastPathSegment?.toIntOrNull()
+                if (id != null) {
+                    val args = Bundle()
+                    args.putInt("movieId", id)
+                    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+                    val navController = navHostFragment.navController
+                    navController.navigate(R.id.action_moviesList_to_movieDetails, args)
+                }
+            }
+        }
     }
 }
